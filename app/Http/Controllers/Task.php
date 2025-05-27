a29dac3663353b3fbb53ad21f5f339c21733b04c
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2025-05-27 15:06:36 -0300

    validating title field

--- app/Http/Controllers/Task.php
+++ app/Http/Controllers/Task.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Task as TaskModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class Task extends Controller
 {
     public function createTask(Request $request){
-        $task = $request->all();
-        $newTask = TaskModel::create($task);
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $validated = $validator->validated();
+
+        $newTask = TaskModel::create($validated);
 
         return response()->json(['task' => $newTask], 201);
     }
